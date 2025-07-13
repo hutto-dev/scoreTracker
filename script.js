be72f548b57937bfbcc613363b7f1a0fe06f72e6
Tori Hutto <183306074+hutto-dev@users.noreply.github.com>
2025-07-12 21:25:59 -0400

    All main components finished

--- script.js
+++ script.js
@@ -17,19 +17,83 @@ const awayRadio = document.getElementById("away-radio");
 const chooseHome = document.getElementById("choose-home");
 const chooseAway = document.getElementById("choose-away");
 
-addTeam.addEventListener("click", function () {
-  event.preventDefault();
+// Choose Team Labels
+const chooseHomeLabel = document.getElementById("choose-home-label");
+const chooseAwayLabel = document.getElementById("choose-away-label");
+
+// Update Score box
+const updateScoreBox = document.getElementById("update-score-box");
+const homeScore = document.getElementById("home-score");
+const awayScore = document.getElementById("away-score");
+
+// Choose Team Buttons
+const submitButton = document.getElementById("submit-button");
+const newGameButton = document.getElementById("new-game-button");
+
+// Modal
+const modalContainer = document.getElementById("modal-container");
+const modalYesButton = document.getElementById("modal-button-yes");
+const modalNoButton = document.getElementById("modal-button-no");
+
+//Add Teams
+
+addTeam.addEventListener("click", function (e) {
+  e.preventDefault();
   if (homeRadio.checked) {
     homeTitle.style.visibility = "visible";
     homeRecord.style.visibility = "visible";
     homeTitle.textContent = teamName.value;
     homeRecord.textContent = teamRecord.value;
+    chooseHomeLabel.textContent = teamName.value;
     teamName.value = "";
     teamRecord.value = "";
+    homeRadio.checked = false;
   } else if (awayRadio.checked) {
     awayTitle.style.visibility = "visible";
     awayRecord.style.visibility = "visible";
     awayTitle.textContent = teamName.value;
     awayRecord.textContent = teamRecord.value;
+    chooseAwayLabel.textContent = teamName.value;
+    teamName.value = "";
+    teamRecord.value = "";
+    awayRadio.checked = false;
   }
+});
+
+// Submitting Scores
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (chooseAway.checked) {
+    awayScore.textContent = updateScoreBox.value;
+    updateScoreBox.value = "";
+  } else if (chooseHome.checked) {
+    homeScore.textContent = updateScoreBox.value;
+    updateScoreBox.value = "";
+  }
+});
+
+// New Games
+
+newGameButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalContainer.style.visibility = "visible";
+});
+
+modalYesButton.addEventListener("click", function () {
+  homeTitle.style.visibility = "hidden";
+  homeRecord.style.visibility = "hidden";
+  homeTitle.textContent = "";
+  homeRecord.textContent = "";
+  chooseAwayLabel.textContent = "Away";
+  chooseHomeLabel.textContent = "Home";
+  teamName.value = "";
+  teamRecord.value = "";
+  awayScore.textContent = "0";
+  homeScore.textContent = "0";
+  modalContainer.style.visibility = "hidden";
+});
+
+modalNoButton.addEventListener("click", function () {
+  modalContainer.style.visibility = "hidden";
 });
